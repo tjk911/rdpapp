f0f61003bcaec4ee9d7d8011a5a33f7c5fb53b83
Kai Teoh <kai@ink-cafe.com>
2014-02-03 13:17:15 -0600

    Relaunch 2.0, fresh start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rdpapp::Application.config.secret_key_base = '92c3209d77ff35d073635b69c261c4bf8f883d4f2b60e67e9230fed27df532ffbe66c42ff89739e6c79b4172f4eb1743271a30d25c00952a4d8211c5971b0c54'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rdpapp::Application.config.session_store :cookie_store, key: '_rdpapp_session'
