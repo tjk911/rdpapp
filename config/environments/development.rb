a5132e03060ae143553cc9cda244298ae82fedbc
Kai Teoh <kai@ink-cafe.com>
2014-02-11 13:03:30 -0600

    Add ssl to production environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,5 +26,5 @@ Rdpapp::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  
+  config.force_ssl = true
 end
