13c95547739836d9ae0f47a0d7d37d98fee412a8
Kai Teoh <kai@ink-cafe.com>
2014-03-04 16:46:12 -0600

    Add tests for valid and invalid signins

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+	
+	def new
+	end
+
+	def create
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+	      # Sign the user in and redirect to the user's show page.
+		else
+	      flash[:warning] = 'Invalid email/password combination' # Not quite right!
+	      render 'new'
+		end
+	end
+
+	def destroy
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rdpapp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root  'static_pages#home'
   get   "static_pages/home"
   get   "static_pages/advertise"
   match "/advertise",   to: "static_pages#advertise",   via: 'get'
   match "/signup",      to: "users#new",                via: 'get'
+  match "/signin",      to: "sessions#new",             via: 'get'
+  match "signout",      to: "sessions#destroy",         via: 'delete'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
