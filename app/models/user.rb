5274d32f2d4af8ccf973d2b853b280641fa5bbae
Kai Teoh <kai@ink-cafe.com>
2014-03-05 11:01:58 -0600

    tweak spec files, add session tokens

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,23 @@
 class User < ActiveRecord::Base
-	before_save { email.downcase! }
+	before_save { self.email= email.downcase }
+	before_create :create_remember_token
 	validates :name, presence: true, length: { maximum: 50 } 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 	validates :password, length: { minimum: 6 }
 	has_secure_password
+
+	def User.new_remember_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def User.hash(token)
+		Digest::SHA1.hexdigest(token.to_s)
+	end
+
+	private
+
+		def create_remember_token
+			self.remember_token = User.hash(User.new_remember_token)
+		end
 end
