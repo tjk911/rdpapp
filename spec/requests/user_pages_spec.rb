95d51ce4abb3fc3a62f5ada44ffb49571a480acf
Kai Teoh <kai@ink-cafe.com>
2014-03-06 11:07:26 -0600

    Start edit user settings

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,9 +4,29 @@ describe "User Pages" do
 	
 	subject { page }
 
+	describe "edit" do
+		let(:user){ FactoryGirl.create(:user) }
+		before{ visit edit_user_path(user) }
+
+		describe "page" do
+			it { should have_content("Update your profile") }
+			it { should have_title("Edit user") }
+			it { should have_link('change', href:'http://gravatar.com/emails') }
+		end
+
+		describe "with invalid information" do
+			before { click_button "Save changes" }
+
+			it { should have_content('error') }
+		end
+	end
+
 	describe "signup page" do
 
 	    before { visit signup_path }
+
+	    it { should have_content("Sign up") }
+		it { should have_title(full_title("Sign up")) }
 
 	    let(:submit) { "Create my account" }
 
@@ -46,14 +66,6 @@ describe "User Pages" do
 	      end
 
 	    end
-
-	end
-
-	describe "signup pages" do
-		before { visit signup_path }
-
-		it { should have_content("Sign up") }
-		it { should have_title(full_title("Sign up")) }
 
 	end
 
