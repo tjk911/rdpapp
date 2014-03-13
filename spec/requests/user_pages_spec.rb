e999d9c4a68ccc0009443049c56814f4eec676ee
Kai Teoh <kai@ink-cafe.com>
2014-03-13 14:59:38 -0500

    Test for valid information

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,18 +6,39 @@ describe "User Pages" do
 
 	describe "edit" do
 		let(:user){ FactoryGirl.create(:user) }
-		before{ visit edit_user_path(user) }
+		before do
+			sign_in user
+			visit edit_user_path(user)
+		end
 
-		describe "page" do
-			it { should have_content("Update your profile") }
-			it { should have_title("Edit user") }
-			it { should have_link('change', href:'http://gravatar.com/emails') }
+		describe "with valid information" do
+			let(:new_name) { "New Name" }
+			let(:new_email) { "new@example.com" }
+			before do
+				fill_in "Name",				with: new_name
+				fill_in "Email",			with: new_email
+				fill_in "Password",			with: user.password
+				fill_in "Confirm Password",	with: user.password
+				click_button "Save changes"
+			end
+
+			it { should have_title(new_name) }
+			it { should have_selector('div.alert-box.success') }
+			it { should have_link("Sign out", href: signout_path) }
+			specify { expect(user.reload.name).to eq new_name }
+			specify { expect(user.reload.email).to eq new_email }
 		end
 
 		describe "with invalid information" do
 			before { click_button "Save changes" }
 
 			it { should have_content('error') }
+		end
+
+		describe "page" do
+			it { should have_content("Update your profile") }
+			it { should have_title("Edit user") }
+			it { should have_link('change', href:'http://gravatar.com/emails') }
 		end
 	end
 
