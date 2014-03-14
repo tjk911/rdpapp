b92573c7e7fb3eeb1f63c88313253beff88d8ffc
Kai Teoh <kai@ink-cafe.com>
2014-03-14 11:40:25 -0500

    Start authorization process

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
 
   def index
     @user = User.all
@@ -41,6 +42,12 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    # before filters
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
     end
 
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,6 +4,26 @@ describe "authentication" do
 
 	subject { page }
 
+	describe "authorization" do
+
+		describe "for non-signed-in users" do
+			let(:user) { FactoryGirl.create(:user) }
+
+			describe "in the Users controller" do
+
+				describe "visiting the edit page" do
+					before { visit edit_user_path(user) }
+					it { should have_title("Sign in") }
+				end
+
+				describe "submitting to the update function" do
+					before { patch user_path(user) }
+					specify { expect(response).to redirect_to(signin_path) }
+				end
+			end
+		end
+	end
+
 	describe "signin" do
 		before { visit signin_path }
 
