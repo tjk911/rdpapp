72e0e9d9628b477c219a9e471ca9478ed31862e1
Kai Teoh <kai@ink-cafe.com>
2014-06-18 20:25:07 -0500

    verify sign in status

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -65,4 +65,15 @@ describe "authentication" do
 		end
 	end
 
+	describe "with valid information" do
+		let(:user) { FactoryGirl.create(:user) }
+		before { sign_in user }
+
+		it { should have_title(user.name) }
+		it { should have_link('Profile', href: user_path(user)) }
+		it { should have_link('Settings', href: edit_user_path(user)) }
+		it { should have_link('Sign out', href:signout_path) }
+		it { should_not have_link('Sign in', href:signin_path) }
+	end
+
 end
