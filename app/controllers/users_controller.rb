b1fa2aa8983ba1cb9c593e23e3e113960ba59090
Kai Teoh <kai@ink-cafe.com>
2014-06-18 20:53:54 -0500

    Implement friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,7 +48,10 @@ class UsersController < ApplicationController
     # before filters
 
     def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
     end
 
     def correct_user
